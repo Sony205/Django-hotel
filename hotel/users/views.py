6f986b78bf398bab945b6ef3f8b449d0bc1f9de7
V1rysn1k <ivan.voronko718@gmail.com>
2026-04-25 10:46:20 +0300

    Add views for users

--- hotel/users/views.py
+++ hotel/users/views.py
@@ -1,3 +1,59 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .forms import RegisterForm, LoginForm
+from .models import CustomUser, AccountInfo
 
-# Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, f'Добро пожаловать, {user.username}!')
+            return redirect('profile')
+    else:
+        form = RegisterForm()
+    return render(request, 'register.html', {'form': form})
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, f'С возвращением, {user.username}!')
+                return redirect('profile')
+            else:
+                messages.error(request, 'Неверное имя пользователя или пароль.')
+        else:
+            messages.error(request, 'Ошибка в форме. Проверьте правильность ввода.')
+    else:
+        form = LoginForm()
+    return render(request, 'login.html', {'form': form})
+
+
+def user_logout(request):
+    logout(request)
+    messages.info(request, 'Вы вышли из системы.')
+    return redirect('login')
+
+
+@login_required(login_url='login')
+def profile(request):
+    """Просмотр своего профиля"""
+    try:
+        account_info = request.user.info
+    except AccountInfo.DoesNotExist:
+        account_info = None
+    
+    return render(request, 'profile.html', {
+        'user': request.user,
+        'account_info': account_info,
+    })
