3924e72d62b1ec89e5cc16419a927c9503635c86
Sony205 <150188460+Sony205@users.noreply.github.com>
2026-04-26 15:20:30 +0300

    изменение views.py + добавление urls.py

--- hotel/rooms/views.py
+++ hotel/rooms/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from .models import Room
 
-# Create your views here.
+
+def room_list(request):
+    rooms = Room.objects.filter(is_available=True)
+
+    return render(request, 'rooms/room_list.html', {
+        'rooms': rooms,
+    })
+
+
+def room_detail(request, room_id):
+    room = get_object_or_404(Room, id=room_id)
+
+    return render(request, 'rooms/room_detail.html', {
+        'room': room,
+    })
